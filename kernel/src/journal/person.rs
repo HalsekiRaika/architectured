e900f4ad9a0e12c958f1141dcc5b05437c6a634b
ReiRokusanami <reirokusanami.rdh@gmail.com>
2024-01-31 10:08:08 +0900

    🔨 - Define specific actions to be performed by the journal.

--- kernel/src/journal/person.rs
+++ kernel/src/journal/person.rs
@@ -1,8 +1,13 @@
 use std::future::Future;
+use crate::entities::{Person, PersonId};
 use crate::event::PersonManipulationEvent;
+use crate::journal::Envelope;
 
 #[orbital::export_service]
 pub trait PersonManipulationEventJournal: 'static + Sync + Send {
     type Error;
-    fn save(&self, event: &PersonManipulationEvent) -> impl Future<Output = Result<(), Self::Error>> + Send;
+    fn create(&self, event: &PersonManipulationEvent) -> impl Future<Output = Result<(), Self::Error>> + Send;
+    fn append(&self, id: &PersonId, event: &PersonManipulationEvent) -> impl Future<Output = Result<(), Self::Error>> + Send;
+    fn replay(&self, id: &PersonId) -> impl Future<Output = Result<Envelope<Person>, Self::Error>> + Send;
+    fn resume(&self, envelope: &mut Envelope<Person>) -> impl Future<Output = Result<(), Self::Error>> + Send;
 }
