62ea816603ef03a9df00fbb58f8cab4900f25d5b
ReiRokusanami <reirokusanami.rdh@gmail.com>
2024-01-31 10:03:16 +0900

    💥 - Changed database architecture.

--- migrations/journal/20240125033918_init.sql
+++ migrations/journal/20240125033918_init.sql
@@ -1,17 +1,16 @@
 CREATE TABLE streams(
-  id      UUID   NOT NULL,
-  version SERIAL NOT NULL CHECK ( version > 0 ) ,
-
-  PRIMARY KEY (id, version)
+  id UUID NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE events(
-  id      INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, -- Auto increment
-  stream  UUID    NOT NULL,
-  version SERIAL  NOT NULL CHECK ( version > 0 ),
-  event   JSON    NOT NULL,
+  id      UUID   NOT NULL DEFAULT gen_random_uuid(), -- Auto increment
+  stream  UUID   NOT NULL,
+  version SERIAL NOT NULL CHECK ( version > 0 ),
+  event   JSON   NOT NULL,
 
   created_at TIMESTAMPTZ NOT NULL DEFAULT clock_timestamp(),
 
-  FOREIGN KEY (stream, version) REFERENCES streams(id, version) ON DELETE CASCADE
+  FOREIGN KEY (stream) REFERENCES streams(id) ON DELETE CASCADE,
+
+  PRIMARY KEY (id)
 );
--- migrations/journal/20240127091511_index.sql
+++ migrations/journal/20240127091511_index.sql
@@ -1,2 +1,2 @@
-CREATE INDEX stream_id_index ON streams(id);
-CREATE INDEX event_id_index  ON events(id);
+CREATE INDEX stream_id_index            ON streams(id);
+CREATE INDEX event_id_with_stream_index ON events(id, stream, version);
