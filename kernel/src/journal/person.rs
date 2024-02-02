54a3229e499654b4cbdd8614d854adac51d3fc4c
ReiRokusanami <reirokusanami.rdh@gmail.com>
2024-02-02 13:53:04 +0900

    🥅 - Changed to use `Report` to take advantage of `error-stack`.

--- kernel/src/journal/person.rs
+++ kernel/src/journal/person.rs
@@ -1,13 +1,14 @@
 use std::future::Future;
+use error_stack::Report;
 use crate::entities::{Person, PersonId};
+use crate::error::KernelError;
 use crate::event::PersonManipulationEvent;
 use crate::journal::Envelope;
 
 #[orbital::export_service]
 pub trait PersonManipulationEventJournal: 'static + Sync + Send {
-    type Error;
-    fn create(&self, event: &PersonManipulationEvent) -> impl Future<Output = Result<(), Self::Error>> + Send;
-    fn append(&self, id: &PersonId, event: &PersonManipulationEvent) -> impl Future<Output = Result<(), Self::Error>> + Send;
-    fn replay(&self, id: &PersonId) -> impl Future<Output = Result<Envelope<Person>, Self::Error>> + Send;
-    fn resume(&self, envelope: &mut Envelope<Person>) -> impl Future<Output = Result<(), Self::Error>> + Send;
+    fn create(&self, event: &PersonManipulationEvent) -> impl Future<Output = Result<(), Report<KernelError>>> + Send;
+    fn append(&self, id: &PersonId, event: &PersonManipulationEvent) -> impl Future<Output = Result<(), Report<KernelError>>> + Send;
+    fn replay(&self, id: &PersonId) -> impl Future<Output = Result<Envelope<Person>, Report<KernelError>>> + Send;
+    fn resume(&self, envelope: &mut Envelope<Person>) -> impl Future<Output = Result<(), Report<KernelError>>> + Send;
 }
